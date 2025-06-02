5e0e54e5b5a685b2f7edcec029ea56a47ab91647
gchakra9 <gourav.chakraborty@ymail.com>
2025-06-02 18:36:55 -0400

    Update App.java

--- src/main/java/org/test/selenium_test/App.java
+++ src/main/java/org/test/selenium_test/App.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 import java.io.File;
 import java.io.IOException;
